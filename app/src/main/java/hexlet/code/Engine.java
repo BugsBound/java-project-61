cbf5d948a280ef44e66d307077182962b2218a77
BugsBound <bugsbound@gmail.com>
2024-07-11 20:07:10 +0300

    MAGIC 4iSLA V SWICHE

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -14,15 +14,15 @@ public class Engine {
         Scanner sc = new Scanner(System.in);
         int selectedGame;
 
-        System.out.print("Please enter the game number and press Enter.\n" +
-                "1 - Greet\n" +
-                "2 - Even\n" +
-                "3 - Calc\n" +
-                "4 - GCD\n" +
-                "5 - Progression\n" +
-                "6 - Prime\n" +
-                "0 - Exit\n" +
-                "Your choice: ");
+        System.out.print("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "2 - Even\n"
+                + "3 - Calc\n"
+                + "4 - GCD\n"
+                + "5 - Progression\n"
+                + "6 - Prime\n"
+                + "0 - Exit\n"
+                + "Your choice: ");
         if (sc.hasNextInt()) {
             selectedGame = sc.nextInt();
             sc.nextLine();
@@ -47,16 +47,39 @@ public class Engine {
             String[] dataRound;
             boolean needPrintRules = i == FIRST_ROUND;
 
-            switch (selectedGame) {
-                case 1 -> {
-                    return;
-                }
-                case 2 -> dataRound = EvenGame.roundGenerator(needPrintRules);
-                case 3 -> dataRound = CalcGame.roundGenerator(needPrintRules);
-                case 4 -> dataRound = NodGame.roundGenerator(needPrintRules);
-                case 5 -> dataRound = ProgressionGame.roundGenerator(needPrintRules);
-                case 6 -> dataRound = PrimeGame.roundGenerator(needPrintRules);
-                default -> throw new Exception("Wrong game number!");
+//            switch (selectedGame) {
+//                case 1 -> {
+//                    return;
+//                }
+//                case 2 -> dataRound = EvenGame.roundGenerator(needPrintRules);
+//                case 3 -> dataRound = CalcGame.roundGenerator(needPrintRules);
+//                case 4 -> dataRound = NodGame.roundGenerator(needPrintRules);
+//                case 5 -> dataRound = ProgressionGame.roundGenerator(needPrintRules);
+//                case 6 -> dataRound = PrimeGame.roundGenerator(needPrintRules);
+//                default -> throw new Exception("Wrong game number!");
+//            }
+
+            int helloGame = 1;
+            int evenGame = 2;
+            int calcGame = 3;
+            int nodGame = 4;
+            int progressionGame = 5;
+            int primeGame = 6;
+
+            if (selectedGame == helloGame) {
+                return;
+            } else if (selectedGame == evenGame) {
+                dataRound = EvenGame.roundGenerator(needPrintRules);
+            } else if (selectedGame == calcGame) {
+                dataRound = CalcGame.roundGenerator(needPrintRules);
+            } else if (selectedGame == nodGame) {
+                NodGame.roundGenerator(needPrintRules);
+            } else if (selectedGame == progressionGame) {
+                dataRound = ProgressionGame.roundGenerator(needPrintRules);
+            } else if (selectedGame == primeGame) {
+                dataRound = PrimeGame.roundGenerator(needPrintRules);
+            } else {
+                throw new Exception("Wrong game number!");
             }
 
             System.out.println("Question: " + dataRound[QUESTION_INDEX]);
@@ -66,8 +89,8 @@ public class Engine {
             if (userAnswer.equals(dataRound[ANSWER_INDEX])) {
                 System.out.println("Correct!");
             } else {
-                System.out.println("'" + userAnswer + "' is wrong answer ;(. " +
-                        "Correct answer was '" + dataRound[ANSWER_INDEX] + "'.");
+                System.out.println("'" + userAnswer + "' is wrong answer ;(. "
+                        + "Correct answer was '" + dataRound[ANSWER_INDEX] + "'.");
                 System.out.println("Let's try again, " + name + "!");
                 sc.close();
                 return;
